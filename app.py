969993c15750ed0b480fd2fa72374b5c1b118eba
nick butzler <nickbutzler1@gmail.com>
2024-03-26 15:02:01 -0500

    wip - partner change

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hell world, index"
+
+@app.route('/nick')
+def nick():
+    n1 = 10 
+    n2 = 20 
+    sum = n1 + n2
+    return str(sum)
+
+if __name__ == '__main__':
+    app.run(debug=True)
