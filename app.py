280f7db67444c67746ce4c1684f0d02301ba9181
Jonathan Mincieli <jmincieli@uiowa.edu>
2024-04-04 15:00:53 -0500

    updates

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "Hello world, index"
+    return render_template("index.html")
 
-@app.route('/nick')
+@app.route("/nick")
 def nick():
     n1 = 10 
     n2 = 20 
     sum = n1 + n2
     return str(sum)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
